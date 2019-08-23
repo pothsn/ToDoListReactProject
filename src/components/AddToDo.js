9fdf704e74131b97fcd9b385ef57fe9e6b61a652
Nate Poths <pothsn@gmail.com>
2019-08-23 14:42:28 -0500

    Created add to do input box and submit button and tweaked some styling.

---
+++ src/components/AddToDo.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class AddToDo extends Component {
+    render() {
+        return (
+            <form style={{ display : 'flex' }}>
+                <input 
+                    type="text"
+                    name="title"
+                    style={{ flex: '10', padding: '5px' }}
+                    placeholder="Add To Do ..."
+                />
+                <input type="submit"
+                    value="submit"
+                    className="btn"
+                    style={{flex: '1'}}
+                />
+            </form>
+        )
+    }
+}
+
+export default AddToDo
