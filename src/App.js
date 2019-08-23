eebb909f7c84854d8d5c10bd8e25870f82dfac84
Nate Poths <pothsn@gmail.com>
2019-08-23 14:58:38 -0500

    Added functionality to add and submit buttons.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddToDo from './components/AddToDo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with fiancee',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Work on project at devCodeCamp',
         completed: false
       },
@@ -41,12 +42,22 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
 
+  // Add To Do
+  AddToDo = (title) => {
+    const newToDo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newToDo] });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddToDo />
+          <AddToDo AddToDo={this.AddToDo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
         </div>
       </div>
