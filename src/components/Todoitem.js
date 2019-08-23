6b1a27246065d4fe2d5fda51d14b5484b11f1465
Nate Poths <pothsn@gmail.com>
2019-08-23 13:55:16 -0500

    Added ability to mark a task as complete.

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -4,22 +4,23 @@ import PropTypes from 'prop-types';
 export class Todoitem extends Component {
     
     getStyle = () => {
-        if (this.props.todo.completed) {
-            return {
-                textDecoration: 'line-through'
-            }
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
         }
-        else{
-            return {
-                textDecoration: 'none'
-            }
-        }
-        
     }
+
     render() {
+        const { id, title } = this.props.todo;
         return (
             <div style={ this.getStyle() }>
-                <p>{this.props.todo.title}</p>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind
+                    (this, id)}/> {' '}
+                    { title }
+                </p>
             </div>
         )
     }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,9 +3,9 @@ import Todoitem from './Todoitem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
-  render() {
+    render() {
     return this.props.todos.map((todo) => (
-        <Todoitem key={todo.id} todo={todo}/>
+        <Todoitem key={todo.id} todo={todo} markComplete={this.props.markComplete}/>
     ));
   }
 }
